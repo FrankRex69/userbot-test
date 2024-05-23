2fbff75dbc6e6b3d0a4cf399099fd229bf1ec756
FrankRex69 <francesco.re69@gmail.com>
2024-05-23 19:36:34 +0200

    feat: setup multer

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import express from 'express';
 
+import multer from 'multer';
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+const upload = multer({ storage })
+
 const app = express();
 const port = 8000
 
@@ -7,9 +18,11 @@ app.get('/', (req, res) => {
   res.send('Hello World !')
 })
 
-app.post('/api/uploadFile', (req, res) => {
-    res.send('Upload file ok !')
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    res.json(req.file)
+    // res.send('Upload file ok !!!')
   })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
